c98551fd7fa96ddfc514377d8132c0151a81c606
Chris Rocca <chrroc2817@students.ecpi.edu>
2022-11-27 12:42:43 -0500

    initial Checkin

---
+++ src/components/App/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import {
+  Route,
+  Routes,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import "./App.css";
+import Home from "../Home/Home.js";
+import Contact from "../Contact/Contact.js";
+import Product from "../Product/Product.js";
+import About from "../About/About.js";
+
+
+class App extends Component {
+    render() {
+      return (
+        <HashRouter>
+          <div> 
+            <h1>Toronto Cupcake</h1>
+            <ul className="header">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+              <li><NavLink to="/product">Products</NavLink></li>      
+              <li><NavLink to="/about">About</NavLink></li> 
+            </ul>
+            <div className="content">
+              <Routes>
+              <Route path="/" element ={<Home/>}/>
+              <Route path="/contact" element ={<Contact/>}/>
+              <Route path="/product" element ={<Product/>}/>
+              <Route path="/about" element ={<About/>}/>
+              </Routes>
+            </div>
+          </div>
+        </HashRouter>
+      );
+    }
+  }
+ 
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import home from "./Home2.png";
+
+export default function Home() {
+    return (
+        <div>
+            <img alt="home" src={home} />
+        </div>
+    )
+}
